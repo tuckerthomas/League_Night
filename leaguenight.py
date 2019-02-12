2268506d9a925a13920d84e16f035eec52d5a743
Tucker Thomas <tuckerthomas@github.com>
2019-02-12 11:07:56 -0500

    Can now add players to different teams

--- leaguenight.py
+++ leaguenight.py
@@ -8,11 +8,10 @@ import requests
 from PySide2.QtWidgets import QApplication, QMessageBox, QListWidgetItem
 from PySide2.QtUiTools import QUiLoader
 from PySide2.QtCore import QFile
-from PySide2.QtGui import QStandardItemModel
 from riot import *
 
 
-def decode_summonerdto_json(dct):
+def decode_summoner_dto_json(dct):
     return SummonerDTO(
         dct['profileIconId'],
         dct['name'],
@@ -25,6 +24,7 @@ def decode_summonerdto_json(dct):
 
 class LeagueNight:
 
+    # before_main() but not actually main() ¯\_(ツ)_/¯
     def __init__(self):
         super().__init__()
 
@@ -35,19 +35,16 @@ class LeagueNight:
 
         self.initUI()
 
+    # main()
     def initUI(self):
         app = QApplication(sys.argv)
 
-        ui_file = QFile("UI/ui_mainwindow_v1.ui")
+        ui_file = QFile("UI/ui_mainwindow.ui")
         ui_file.open(QFile.ReadOnly)
 
         loader = QUiLoader()
         self.window = loader.load(ui_file)
         ui_file.close()
-
-        self.player_pool_model = (1, 5)
-
-        ## self.window.playerPoolList() TODO: Start here
 
         self.window.usernameEdit.textEdited.connect(self.enable_add_player)
         self.window.usernameEdit.returnPressed.connect(self.add_player)
@@ -57,8 +54,8 @@ class LeagueNight:
         self.window.putTeam1Button.clicked.connect(self.add_player_team1)
         self.window.putTeam2Button.clicked.connect(self.add_player_team2)
 
-        # self.window.removePlayerTeam1Button.clicked.connect(self.remove_player_team1)
-        # self.window.removePlayerTeam2Button.clicked.connect(self.remove_player_team2)
+        self.window.removePlayerTeam1Button.clicked.connect(self.remove_player_team1)
+        self.window.removePlayerTeam2Button.clicked.connect(self.remove_player_team2)
 
         # Other Stuff
         self.start_time = time.time()
@@ -73,80 +70,107 @@ class LeagueNight:
             QMessageBox.warning(
                 self.window, "Error", "Enter Summoner Name", QMessageBox.Ok)
 
-    def create_player(self, parent):
+    # Creates player widget for player pool list
+    def create_player(self, parent, summoner):
         loader = QUiLoader()
 
         file = QFile("UI/ui_playerpoolwidget.ui")
         file.open(QFile.ReadOnly)
 
-        return loader.load(file, parent)
+        new_player = loader.load(file, parent)
+        file.close()
 
+        # Set the QWidget's summoner
+        new_player.summoner = summoner
+
+        return new_player
+
+    # Adds a new player to the player pool list
     def add_player(self):
-
         summoner = self.get_summoner(self.window.usernameEdit.text())
 
         if summoner is None:
             QMessageBox.warning(self.window, "League Night", "Invalid Summoner Name", QMessageBox.Ok, QMessageBox.NoButton)
             return
 
-        item = QStandardItemModel()
         item = QListWidgetItem(self.window.playerPoolList)
 
-        self.playerWidget = self.create_player(self.window.playerPoolList)
-        self.playerWidget.playerLabel.setText(summoner.name)
-        self.playerWidget.pointLabel.setText(str(summoner.points))
+        playerWidget = self.create_player(self.window.playerPoolList, summoner)
+        playerWidget.playerLabel.setText(summoner.name)
+        playerWidget.pointLabel.setText(str(summoner.points))
 
-        item.setSizeHint(self.playerWidget.sizeHint())
+        item.setSizeHint(playerWidget.sizeHint())
 
         self.window.playerPoolList.addItem(item)
-        self.window.playerPoolList.setItemWidget(item, self.playerWidget)
+        self.window.playerPoolList.setItemWidget(item, playerWidget)
 
         self.window.usernameEdit.setText("")
         self.window.addPlayerButton.setEnabled(False)
 
-    def remove_player_pool_widget(self):
-        self.window.playerPoolList.removeItemWidget(self)
-
+    # Creates player widget for team lists
     def add_player_to_team(self, parent):
         file = QFile("UI/ui_playerteamwidget.ui")
         file.open(QFile.ReadOnly)
         loader = QUiLoader()
+
         PlayerTeamWidget = loader.load(file, parent)
         file.close()
 
         return PlayerTeamWidget
 
-    # TODO: START HERE
+    # Adds a player from the Player Pool to the Team 1 List
     def add_player_team1(self):
-        poolList = self.window.playerPoolList.count()
-        item = self.window.playerPoolList.item(0)
-        players = self.window.playerPoolList.selectedItems()
-        for index in players:
-            player = self.window.team1List.itemWidget(index)
-            item = QListWidgetItem(self.window.team1List)
+        team1List = self.window.team1List
+        playerPoolList = self.window.playerPoolList
 
-            team1Player = self.add_player_to_team(self.window.team1List)
+        players = playerPoolList.selectedItems()
+
+        for index in players:
+            player = playerPoolList.itemWidget(index)
+            item = QListWidgetItem(team1List)
+
+            team1Player = self.add_player_to_team(team1List)
             team1Player.playerLabel.setText(player.summoner.name)
 
             item.setSizeHint(team1Player.sizeHint())
-            self.window.team1List.addItem(item)
-            self.window.team1List.setItemWidget(item, team1Player)
+            team1List.addItem(item)
+            team1List.setItemWidget(item, team1Player)
 
+    # Adds a player from the Player Pool to the Team 2 List
     def add_player_team2(self):
-        for player in self.window.playerPoolList.selectedItems():
-            item = QListWidgetItem(self.window.team2List)
+        team2List = self.window.team2List
+        playerPoolList = self.window.playerPoolList
 
-            team2Player = self.add_player_to_team(self.window.team2List)
+        players = playerPoolList.selectedItems()
+
+        for index in players:
+            player = playerPoolList.itemWidget(index)
+            item = QListWidgetItem(team2List)
+
+            team2Player = self.add_player_to_team(team2List)
             team2Player.playerLabel.setText(player.summoner.name)
 
             item.setSizeHint(team2Player.sizeHint())
-            self.window.team2List.addItem(item)
-            self.window.team2List.setItemWidget(item, team2Player)
+            team2List.addItem(item)
+            team2List.setItemWidget(item, team2Player)
+
+    # Removes a player from the Team 1 List
+    def remove_player_team1(self):
+        team1List = self.window.team1List
+
+        for item in team1List.selectedItems():
+            team1List.takeItem(team1List.row(item))
+
+    # Removes a player from the Team 2 List
+    def remove_player_team2(self):
+        team2List = self.window.team2List
+
+        for item in team2List.selectedItems():
+            team2List.takeItem(team2List.row(item))
 
     def get_summoner(self, username: str):
         summoner_regex = re.compile('^[0-9\w+ _\.]+$', re.UNICODE)
         summoner_name = username
-        summoner = None
 
         url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summoner_name
         payload = {'api_key': self.api_key}
@@ -158,7 +182,7 @@ class LeagueNight:
 
             if summoner_name_request.status_code == 200:
                 summoner = json.loads(
-                    summoner_name_request.text, object_hook=decode_summonerdto_json)
+                    summoner_name_request.text, object_hook=decode_summoner_dto_json)
                 return summoner
             else:
                 return None
